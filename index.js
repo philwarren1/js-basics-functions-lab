1c4d4c6d3e6990ac43f76493e387879ae03a1405
Philip <philwarren1@gmail.com>
2021-04-07 14:16:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance > 42){
+      return distance -42
+      } else {
+        return 42 - distance
+      }
+  }
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+  }
+
+function distanceTravelledInFeet(start, end){
+    if (start > end){
+      return (start - end) * 264
+    } else{
+      return (end - start) * 264
+    }
+} 
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end) 
+        if (distance < 400)
+        {return 0}
+        else if (distance > 400 && distance <= 2000)
+          {return (distance - 400) * .02}
+        else if (distance > 2000 && distance <= 2500)
+          {return 25}
+         else 
+          {return 'cannot travel that far';}
+ 
+        }
